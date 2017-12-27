75a11d957b808ea24fcd81837ad46e409e7f7de3
amine <amine@hot.com>
2017-12-27 23:29:55 +0000

    1-First commit create projet \n 2-Change de name of app in String.xml\n 3-Change de color of toolbar in color.xml\n

---
+++ app/src/main/java/com/amine/hiddenfoundrs/login/LoginActivity.java
@@ -0,0 +1,57 @@
+package com.amine.hiddenfoundrs.login;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.amine.hiddenfoundrs.R;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private com.facebook.login.widget.LoginButton loginButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+
+
+
+            if (fragment == null) {
+
+
+
+//Generation KEY HASH
+                try {
+                    PackageInfo info = getPackageManager().getPackageInfo("com.amine.hiddenfoundrs",         PackageManager.GET_SIGNATURES);
+                    for (Signature signature : info.signatures) {
+                        MessageDigest md = MessageDigest.getInstance("SHA");
+                        md.update(signature.toByteArray());
+                        String sign= Base64.encodeToString(md.digest(), Base64.DEFAULT);
+                        Log.e("MY KEY HASH:", sign);
+                  //      Toast.makeText(getApplicationContext(),sign,         Toast.LENGTH_LONG).show();
+                    }
+                } catch (PackageManager.NameNotFoundException e) {
+                } catch (NoSuchAlgorithmException e) {
+                }
+
+                fragment = new FacebookFragment();
+                fm.beginTransaction()
+                        .add(R.id.fragment_container, fragment)
+                        .commit();
+            }
+
+    }
+}
